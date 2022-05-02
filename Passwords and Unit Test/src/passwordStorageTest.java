644de92d2ba61e6568e1675c687bff165639341a
Megan Coats <ctsmgn@yahoo.com>
2022-05-02 18:27:43 -0400

    Add Lab

--- Passwords and Unit Test/src/passwordStorageTest.java
+++ Passwords and Unit Test/src/passwordStorageTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -5,8 +6,22 @@ import org.junit.jupiter.api.Test;
 class passwordStorageTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBook() {
+		boolean expected = true;
+		boolean actual = passwordStorage.addPassword("BooK");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void testBOOK() {
+		boolean expected = true;
+		boolean actual = passwordStorage.addPassword("BOOK");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = true;
+		boolean actual = passwordStorage.addPassword("BOOK6");
+		assertEquals("You screwed up", expected, actual);
 	}
 
 }
