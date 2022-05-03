e93284b112294eb69b21c3e0d8e184d639eed911
Megan Coats <ctsmgn@yahoo.com>
2022-05-02 20:03:00 -0400

    Add Lab

--- Passwords and Unit Test/src/passwordStorageTest.java
+++ Passwords and Unit Test/src/passwordStorageTest.java
@@ -7,20 +7,56 @@ class passwordStorageTest {
 
 	@Test
 	void testBook() {
-		boolean expected = true;
-		boolean actual = passwordStorage.addPassword("BooK");
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BooK123");
 		assertEquals("You screwed up", expected, actual);
 	}
 	@Test
 	void testBOOK() {
 		boolean expected = true;
-		boolean actual = passwordStorage.addPassword("BOOK");
+		boolean actual = passwordStorage.addPassword("BOOK123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
 		assertEquals("You screwed up", expected, actual);
 	}
 	@Test
 	void test6() {
-		boolean expected = true;
-		boolean actual = passwordStorage.addPassword("BOOK6");
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
+		assertEquals("You screwed up", expected, actual);
+	}
+	@Test
+	void test6() {
+		boolean expected = false;
+		boolean actual = passwordStorage.addPassword("BOOK6123");
 		assertEquals("You screwed up", expected, actual);
 	}
 
